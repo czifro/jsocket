3c851431ea9f068a704bb054ed8b664506cb5c0a
Will Czifro <czifro@gmail.com>
2014-12-30 15:50:54 -0800

    Changed JSocket from abstract to capable of instantiation. JSocket now has basic functions.

--- jsocket/src/net/jsock/JSocket.java
+++ jsocket/src/net/jsock/JSocket.java
@@ -5,15 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.ByteBuffer;
 
 /**
  * Created by czifro on 12/29/14.
  * @author Will Czifro
  * @version 0.1.0
  *
- * An abstraction of a TCP Socket, abstract class
+ * A wrapper for Socket connections
  */
-public abstract class JSocket {
+public class JSocket {
 
     protected PrintWriter writer;
     protected BufferedReader reader;
@@ -25,7 +26,59 @@ public abstract class JSocket {
         reader = new BufferedReader(new InputStreamReader(this.conn.getInputStream()));
     }
 
-    protected abstract byte[] recv();
-    protected abstract byte[] recv_all(int size);
-    protected abstract void send(char[] s);
+    /**
+     * Receives message from client in bytes
+     *
+     * @return byte[],  bytes sent by client
+     */
+    public byte[] recv(){
+        String msg = "";
+        char[] data = new char[2048];
+        byte[] byData = new byte[4096];
+        try{
+            reader.read(data);     // reads bytes into char[]
+            ByteBuffer.wrap(byData).asCharBuffer().put(data);  // converts char[] to byte[]
+        }catch(IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+        return byData;
+    }
+
+    /**
+     * Receives message from client in bytes
+     *
+     * @param size, buffer size
+     * @return
+     */
+    public byte[] recv_all(int size)
+    {
+        char[] data = new char[size];
+        byte[] byData = new byte[size];
+        try{
+            reader.read(data);     // reads bytes into char[]
+            ByteBuffer.wrap(byData).asCharBuffer().put(data);  // converts char[] to byte[]
+        }catch(IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+        return byData;
+    }
+
+    public void send(byte[] b)
+    {
+        char[] s = b.toString().toCharArray();
+        writer.print(s);
+    }
+
+    /**
+     * Closes Socket connection and input and output streams
+     */
+    public void close(){
+        try {
+            conn.close();
+            writer.close();
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- jsocket/src/net/jsock/MessageSocket.java
+++ jsocket/src/net/jsock/MessageSocket.java
@@ -4,7 +4,6 @@ package net.jsock;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.Socket;
-import java.nio.ByteBuffer;
 
 /**
 * Created by czifro on 12/29/14
@@ -21,11 +20,11 @@ public class MessageSocket extends JSocket {
         super(conn);
     }
 
-    /*
-    * Removes '\0' characters from string
-    * @param String,  message to be rebuilt
-    * @return String,  rebuilt message
-    */
+    /**
+     * Removes '\0' characters from string
+     * @param msg,  message to be rebuilt
+     * @return String,  rebuilt message
+     */
     private static String constructCleanMessage(String msg){
         String temp = "";
         for (char c : msg.toCharArray()){
@@ -62,63 +61,14 @@ public class MessageSocket extends JSocket {
         return msg;
     }
 
-    /**
-     * Receives message from client in bytes
-     *
-     * @return byte[],  bytes sent by client
-     */
-    @Override
-    protected byte[] recv(){
-        String msg = "";
-        char[] data = new char[2048];
-        byte[] byData = new byte[4096];
-        try{
-            reader.read(data);     // reads bytes into char[]
-            ByteBuffer.wrap(byData).asCharBuffer().put(data);  // converts char[] to byte[]
-        }catch(IOException ioe){
-            System.out.println(ioe.getMessage());
-        }
-        return byData;
-    }
-
-    @Override
-    protected byte[] recv_all(int size)
-    {
-        char[] data = new char[size];
-        byte[] byData = new byte[size];
-        try{
-            reader.read(data);     // reads bytes into char[]
-            ByteBuffer.wrap(byData).asCharBuffer().put(data);  // converts char[] to byte[]
-        }catch(IOException ioe){
-            System.out.println(ioe.getMessage());
-        }
-        return byData;
-    }
 
     /**
      * Writes message to stream and sends it
      * @param msg,  a String to be sent to client
      */
     public void send_msg(String msg){
-        send(msg.toCharArray());
+        send(msg.getBytes());
     }
 
-    @Override
-    protected void send(char[] s)
-    {
-        writer.print(s);
-    }
 
-    /*
-    * Closes Socket connection and input and output streams
-    */
-    public void close(){
-        try {
-            conn.close();
-            writer.close();
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
